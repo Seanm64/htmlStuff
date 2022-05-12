b32799d40a13b55dfb5325135c604b0e2819e0de
Sean <51032338+Seanm64@users.noreply.github.com>
2022-05-12 13:36:07 -0400

    Digital Clock
    Added Clock at bottom

--- Clock/script.js
+++ Clock/script.js
@@ -3,6 +3,7 @@ setInterval(setClock, 1000)
 const hourHand = document.querySelector('[data-hour-hand]')
 const minuteHand = document.querySelector('[data-minute-hand]')
 const secondHand = document.querySelector('[data-second-hand]')
+const analogClock = document.querySelector('[bottom-clock-id]')
 
 function setClock()
 {
@@ -13,7 +14,33 @@ function setClock()
     setRotation(secondHand, secondsRatio)
     setRotation(minuteHand, minutesRatio)
     setRotation(hourHand, hoursRatio)
+    setAnalog()
 }
+
+function setAnalog()
+{
+    const currentDate = new Date()
+    let h = currentDate.getHours()
+    let m = currentDate.getMinutes()
+    let s = currentDate.getSeconds()
+
+    h = checkHour(h)
+    m = checkTime(m)
+    s = checkTime(s)
+
+    analogClock.innerText = h + ":" + m + ":" + s;
+}
+
+
+function checkHour(h)
+{
+    return (h > 12) ? h - 12 : h
+}
+
+function checkTime(i) {
+    if (i < 10) {i = "0" + i}  // add zero in front of numbers < 10
+    return i;
+  }
 
 function setRotation(element, rotationRatio)
 {
